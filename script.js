2ff9019feaff76bc1068fae9586234b0eeb73f46
aysznsy <ayseguluzunsoycontact@gmail.com>
2025-03-21 01:07:52 +0300

    Clean the unnecessary files from week1

--- script.js
+++ script.js
@@ -9,25 +9,4 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("showLoginBtn").classList.remove("hidden");
   });
 
-  function updateClock() {
-      const now = new Date();
-      const hours = now.getHours().toString().padStart(2, '0');
-      const minutes = now.getMinutes().toString().padStart(2, '0');
-      const seconds = now.getSeconds().toString().padStart(2, '0');
-      document.getElementById("liveClock").textContent = `${hours}:${minutes}:${seconds}`;
-  }
-  setInterval(updateClock, 1000);
-  updateClock();
-
-  document.addEventListener("keydown", function(event) {
-    if (event.key === "h" || event.key === "H") {
-        
-        
-      const elementsToHide = document.querySelectorAll("form, #liveClock, img, #closeLoginBtn, #loginBackground");
-        
-        elementsToHide.forEach(element => {
-            element.classList.toggle("hidden");
-        });
-    }
-});
 });
